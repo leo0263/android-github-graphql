49c231cd38bd5fee04df7b4f6f39f5eecb1b7c44
leo0263 <leo.0263@gmail.com>
2024-10-30 13:30:18 +0700

    Fix font styling

--- app/src/main/java/com/leo0263/cobagithub/ui/search/SearchScreen.kt
+++ app/src/main/java/com/leo0263/cobagithub/ui/search/SearchScreen.kt
@@ -2,7 +2,6 @@ package com.leo0263.cobagithub.ui.search
 
 import android.util.Log
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -14,6 +13,7 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
@@ -24,7 +24,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
@@ -102,9 +101,9 @@ fun UserListItem(user: GithubUserProfile, onClick: () -> Unit = {}) {
         )
         Spacer(modifier = Modifier.width(8.dp))
         Column {
-            Text(text = user.login, fontWeight = FontWeight.Bold)
-            Text(text = user.name)
-            Text(text = user.bio, maxLines = 1, overflow = TextOverflow.Ellipsis)
+            Text(text = user.login, style = MaterialTheme.typography.bodyMedium)
+            Text(text = user.name, style = MaterialTheme.typography.bodySmall, color = Color.Gray)
+            Text(text = user.bio, maxLines = 1, overflow = TextOverflow.Ellipsis, style = MaterialTheme.typography.bodySmall)
         }
     }
 }
